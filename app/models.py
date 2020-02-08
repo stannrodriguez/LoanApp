81efd7a4c4592aa96ae9e462004c4bd15f0173f9
Stephanie Rodriguez <stephsiriban@gmail.com>
2020-02-07 22:24:51 -0800

    Add Strategy Page and Update CSS

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from app import db
+from datetime import datetime
+import numpy as np
+import pandas as pd
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +24,47 @@ class Income(db.Model):
 	income = db.Column(db.Float)
 	income_pct = db.Column(db.Float)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+def loan_schedule(loan, payment=1000, start_date=datetime.now()):
+	"""
+	Create amortization schedule for loan.
+	loan: dict or DataFrame with the following keys or columns
+		1) name 2) int_rate 3) balance
+	payment: float or int, Default 1000
+	start_date (optional): datetime object
+	"""
+
+	# Initialize variables and dataframe
+	schedule = pd.DataFrame()
+	num_payments = np.nper(loan['int_rate']/(100*12), -1000, loan['balance'])
+	dates = pd.date_range(start=start_date,freq='M',periods=np.ceil(num_payments))
+	int_rate, initial_balance = loan['int_rate']/100, loan['balance']
+
+	# For each date, calculate interest and principal payment until balance is 0
+	for date in dates:
+		interest_payment = initial_balance*int_rate/12
+		remaining_balance = initial_balance*(1+int_rate/12)-payment
+		principal_payment = payment - interest_payment
+		if payment > initial_balance + interest_payment:
+		  principal_payment = initial_balance
+		  remaining_balance = 0
+		  payment = principal_payment + interest_payment
+		schedule = schedule.append([
+		    {
+		        'Date': date,
+		        'Initial Balance': initial_balance,
+		        'Payment': payment,
+		        'Interest Payment': interest_payment,
+		        'Principal Payment': principal_payment,
+		        'Remaining Balance': remaining_balance
+		    }
+		])
+		initial_balance = remaining_balance
+	# Insert Loan Information
+	schedule.insert(1, 'Name', loan['name'])
+	schedule.insert(2, 'Interest Rate', loan['int_rate'])
+	# Clean Table
+	schedule['Date'] = schedule['Date'].dt.strftime('%m/%d/%Y')
+	schedule = schedule.set_index(['Date','Name']).round(2).reset_index()
+	return schedule
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, send_file
 from app import app, db
 from app.forms import LoanForm, IncomeForm
-# from app.models import User, Loan
+from app.models import loan_schedule
+import plotly.express as px
+import numpy as np
+import pandas as pd
 
+# from app.models import User, Loan
+global user
+user = {}
 @app.route('/')
 def index():
 	return render_template('base.html', title='Pay It Off')
@@ -12,43 +18,62 @@ def loans():
 	# user = User()
 	# db.session.add(user)
 	# db.session.commit()
-	loans = []
+
 	form = LoanForm()
 	if form.validate_on_submit():
-		# loan = Loan(name=form.name.data, balance=form.balance.data,
-		# 			int_rate=form.int_rate.data, user=user)
-		flash('Your loan data has been saved!')
-		# loans = user.loans.all()
-		loans = [
-			{
-				'name': 'Chase',
-				'balance': 27808.80,
-				'int_rate': 8.615
-			},
-			{
-				'name': 'Sallie Mae',
-				'balance': 54186.21,
-				'int_rate': 10.125
+		current_loan = 	{
+				'name': form.name.data,
+			 	'balance': form.balance.data,
+			 	'int_rate': form.int_rate.data
 			}
-		]
-		return render_template('loans.html', form=form, loans=loans)
-
-	return render_template('loans.html', form=form, loans=loans)
+		try:
+			loan_data.append(current_loan)
+		except NameError:
+			loan_data = [current_loan]
+		user['loan_data'] = loan_data
+		flash('Your loan data has been saved!')
+		return render_template('loans.html', form=form, loans=loan_data)
+	else:
+		return render_template('loans.html', form=form)
 
 @app.route('/income', methods=['GET', 'POST'])
-def income():
+def income(user):
 	income_form = IncomeForm()
 	income = [
 		{
 			'income': 60000,
-			'income_pct': 20}
+			'income_pct': 20
+		}
 	]
 
 	if income_form.validate_on_submit():
-		return render_template('strategy.html')
+		income_data = {'income': income_form.income.data,
+					   'income_pct': income_form.income_pct.data}
+		user['income_data'] = income_data
+		return redirect(url_for('strategy'))
 
 	return render_template('income.html', form=income_form, income=income)
 
-@app.route('/strategy')
+@app.route('/strategy', methods=['GET'])
 def strategy():
-	return render_template('strategy.html')
+	user = {
+		'loan_data':[{
+		'name': 'Chase',
+		'balance': 50000,
+		'int_rate': 8
+		}
+		]
+	}
+	schedule = loan_schedule(user['loan_data'][0])
+	schedule.to_csv('app/DownloadFiles/LoanStrategy.csv')
+	chart = px.area(schedule, x='Date', y='Remaining Balance', color='Name')
+
+	return render_template('strategy.html',
+							chart=chart.to_html(full_html=False), 
+							data_preview=schedule.head().to_html(classes=['table']))
+
+@app.route('/download')
+def download():
+	return send_file('DownloadFiles/LoanStrategy.csv',
+		mimetype='text/csv', attachment_filename='LoanStrategy.csv',
+		as_attachment=True)
